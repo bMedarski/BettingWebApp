9d629945a138d9a589f9307c995304ad157f7339
doBRoDuSHKo\BoyanMedarski <boyamedarski@mail.bg>
2018-12-11 11:38:07 +0200

    Added Moderation and Administration areas

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -87,6 +87,10 @@ namespace Web
 			app.UseMvc(routes =>
 			{
 				routes.MapRoute(
+					name: "areas",
+					template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+				);
+				routes.MapRoute(
 					name: "default",
 					template: "{controller=Home}/{action=Index}/{id?}");
 			});
